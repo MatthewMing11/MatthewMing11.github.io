f64a02056280a87dc41f2cc0afe4dd93172008a9
MatthewMing11 <matthew.ming11@myhunter.cuny.edu>
2024-12-31 22:54:40 -0500

    fixed config for deployment to github pages

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -10,16 +10,16 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://matthewming11.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'matthewming11', // Usually your GitHub org/user name.
+  projectName: 'Portfolio-Website', // Usually your repo name.
+  trailingSlash:False,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
